95f335b1197fc9f1352d790c893135b2fce6ddea
ImenBrahim <imen.brahim@esprit.tn>
2019-09-09 11:52:47 +0100

    Update HasLateBooksException.java

--- src/main/java/fr/d2factory/libraryapp/library/HasLateBooksException.java
+++ src/main/java/fr/d2factory/libraryapp/library/HasLateBooksException.java
@@ -1,7 +1,15 @@
 package fr.d2factory.libraryapp.library;
 
+import fr.d2factory.libraryapp.member.Member;
+
 /**
  * This exception is thrown when a member who owns late books tries to borrow another book
  */
 public class HasLateBooksException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public HasLateBooksException(Member member) {
+        System.out.println("Member is Late");
+    }
 }
+
