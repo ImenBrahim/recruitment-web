e522fc39550be41e5330e5929abda031e3deddf5
ImenBrahim <imen.brahim@esprit.tn>
2019-09-09 11:51:40 +0100

    Update BookRepository.java

--- src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
+++ src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
@@ -1,9 +1,15 @@
 package fr.d2factory.libraryapp.book;
 
+import fr.d2factory.libraryapp.library.HasLateBooksException;
+import fr.d2factory.libraryapp.member.Member;
+import fr.d2factory.libraryapp.member.ResidentMembers;
+import fr.d2factory.libraryapp.member.StudentMembers;
+
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 /**
  * The book repository emulates a database via 2 HashMaps
@@ -13,20 +19,87 @@ public class BookRepository {
     private Map<Book, LocalDate> borrowedBooks = new HashMap<>();
 
     public void addBooks(List<Book> books){
-        //TODO implement the missing feature
+
+        if (books.size() > 0) {
+            for (Book book : books) {
+                getAvailableBooks().put(book.getIsbn(), book);
+            }
+        }
     }
 
+
     public Book findBook(long isbnCode) {
-        //TODO implement the missing feature
+        for (Map.Entry<ISBN, Book> entry : availableBooks.entrySet()) {
+            if (entry.getKey().getIsbnCode() == isbnCode)
+                return entry.getValue();
+        }
+
         return null;
     }
 
-    public void saveBookBorrow(Book book, LocalDate borrowedAt){
-        //TODO implement the missing feature
+    public Map<ISBN, Book> getAvailableBooks() {
+        return availableBooks;
+    }
+
+    public void setAvailableBooks(Map<ISBN, Book> availableBooks) {
+        this.availableBooks = availableBooks;
+    }
+
+    public Map<Book, LocalDate> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(Map<Book, LocalDate> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
+
+    public void saveBookBorrow(Book book, LocalDate borrowedAt) {
+        if (book != null) {
+            if (borrowedAt != null) {
+                getBorrowedBooks().put(book, borrowedAt);
+                getAvailableBooks().remove(book.getIsbn(), book);
+            } else {
+                getBorrowedBooks().remove(book);
+                getAvailableBooks().put(book.getIsbn(), book);
+            }
+        }
+
     }
 
     public LocalDate findBorrowedBookDate(Book book) {
-        //TODO implement the missing feature
-        return null;
+
+        return getBorrowedBooks().get(book);
+    }
+
+
+    public Book borrowBook(long isbnCode, Member member, LocalDate borrowedAt) throws HasLateBooksException {
+        Book book = findBook(isbnCode);
+        if (!member.isLate()) {
+            if (book != null) {
+                saveBookBorrow(findBook(isbnCode), borrowedAt);
+            }
+        } else {
+            throw new HasLateBooksException(member);
+        }
+        return book;
+    }
+
+
+    public void returnBookBecauseOfLate(Book book, Member member, int numbersOfDays) {
+
+        boolean isLate  = false;
+        if (member instanceof StudentMembers) {
+
+            if(numbersOfDays>30)
+                isLate =true;
+
+
+        }else if(member instanceof ResidentMembers){
+            if(numbersOfDays>60)
+                isLate =true;
+        }
+
+        member.payBook(numbersOfDays,isLate);
+
     }
 }
